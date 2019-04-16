40c039916d6d2968d05e84d58e3f1daa02d700f4
eslam elaraby <islam.ismail038@gmail.com>
2019-04-16 02:51:18 +0200

    first weather app commit

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = props => {
+  return (
+    <form onSubmit={props.getWeather}>
+      <input type="text" name="city" placeholder=" Enter city name..." />
+      <input type="text" name="country" placeholder="Enter country name..." />
+      <button>Get Weather</button>
+    </form>
+  );
+};
+
+export default Form;
